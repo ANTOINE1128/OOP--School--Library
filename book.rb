e79fe9e9a55134c7491f1ecacad1082afd0f4422
forLoop94 <charlesuwem94@gmail.com>
2023-08-09 09:51:04 +0100

    Refactor create-book method in App class to display properly

--- book.rb
+++ book.rb
@@ -3,7 +3,7 @@ class Book
   attr_reader :rentals
 
   def initialize(title = 'no title', author = 'no author')
-    @id = id.nil? ? Random.rand(100..10_000) : id
+    @id = Random.rand(100..10_000)
     @title = title
     @author = author
     @rentals = []
@@ -11,32 +11,5 @@ class Book
 
   def add_rental(person, date)
     Rental.new(date, person, self)
-  end
-
-  def to_s
-    "📚 Title: #{@title} by #{@author}"
-  end
-
-  def to_json(*_args)
-    JSON.dump({
-                id: @id,
-                title: @title,
-                author: @author
-              })
-  end
-
-  def self.from_json(string)
-    data = JSON.parse string
-    new(id: data['id'], title: data['title'], author: data['author'])
-  end
-
-  # self.select is a class method
-  #   it receives an id and an array of books
-  def self.select(book_id, books)
-    # books.select will do the following
-    #   - search for a book with id of book_id
-    #   - in case a book with that id exists
-    #      - it will be returned using the .first method
-    books.select { |book| book.id == book_id }.first
   end
 end
