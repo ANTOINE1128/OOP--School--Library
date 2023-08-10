8594874fdaf4a720fa390465140fb75e44177583
forLoop94 <charlesuwem94@gmail.com>
2023-08-10 04:10:51 +0100

    Create a storage class and add methods that check the existenceof dir and files and creates if they are unavailable

---
+++ storage.rb
@@ -0,0 +1,21 @@
+require 'json'
+
+module Storage
+  def storage_directory_check?
+    Dir.exist?('storage')
+  end
+
+  def create_storage_directory
+    Dir.mkdir('storage')
+  end
+
+  def file_check?(name)
+    File.exist?("#{name}.json")
+  end
+
+  def create_file(name, data)
+    File.open("storage/#{name}.json", "w") do |f|
+      f.write(JSON.pretty_generate(data))
+    end
+  end
+end
